8d475ba1b85641bc1a7159886a50fc0e519d0665
Raheltati <125449663+Raheltati@users.noreply.github.com>
2023-04-21 17:48:34 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT UNSIGNED NUMBER *************************/
+/************************* PRINT UNSIGNED NUMBER *********************/
 /**
  * print_unsigned - Should Print an unsigned number
  * @types: List a of arguments
